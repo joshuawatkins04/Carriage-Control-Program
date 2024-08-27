3ff7db4375d7fe6b1dcd2c026416f0a4da63b0b3
Joshua Watkins <joshua@watkinsfamily.com.au>
2024-08-27 15:35:46 +1000

    Simulated an MCP server to receive UDP packets from a test CCP client. Implemented testing for this as well.

---
+++ CCP/src/CCP_TEST_CLIENT.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.net.*;
+
+/*
+ * Test CCP client that will set up a connection between MCP and CCP and then has a 
+ * function to send UDP messages
+ * 
+ * Will also need to set up a function to manage requests from the MCP
+ */
+public class CCP_TEST_CLIENT {
+    
+    private final DatagramSocket socket;
+    private final InetAddress address;
+    private byte[] buf;
+
+    public CCP_TEST_CLIENT() throws SocketException, UnknownHostException {
+        socket = new DatagramSocket();
+        address = InetAddress.getByName("localhost");
+    }
+
+    // Starts server
+    public void start() throws SocketException {
+        MCP_TEST_SERVER server = new MCP_TEST_SERVER();
+        server.start();
+    }
+
+    // Send UPD packet as a String. Needs to change so it sends JSON message
+    public String sendEcho(String msg) throws IOException {
+        buf = msg.getBytes();
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 4445);
+        socket.send(packet);
+        packet = new DatagramPacket(buf, buf.length);
+        socket.receive(packet);
+        String received = new String(packet.getData(), 0, packet.getLength());
+        return received;
+    }
+
+    // Closes socket
+    public void close() {
+        socket.close();
+    }
+}
--- CCP/src/Offboard_Master_CCP.java
+++ CCP/src/Offboard_Master_CCP.java
@@ -8,15 +8,21 @@ public class Offboard_Master_CCP {
     public static void main(String[] args) throws Exception {
         CCP_Connection_Man ccp_connection_man = new CCP_Connection_Man();
 
-        System.out.println("\n\nINITIALISING PHASE:\n");
-        System.out.println("Status of IR_Sensor: " + ccp_connection_man.get_IS_Status());
-        System.out.println("Status of Motor_Controller: " + ccp_connection_man.get_MC_Status());
-        System.out.println("Status of Servo_Controller: " + ccp_connection_man.get_SC_Status());
-        System.out.println("Carriage Status: " + ccp_connection_man.send_Carriage_Status());
-        System.out.println("Door State: " + ccp_connection_man.get_Door_State());
+        // System.out.println("\n\nINITIALISING PHASE:\n");
+        // System.out.println("Status of IR_Sensor: " + ccp_connection_man.get_IS_Status());
+        // System.out.println("Status of Motor_Controller: " + ccp_connection_man.get_MC_Status());
+        // System.out.println("Status of Servo_Controller: " + ccp_connection_man.get_SC_Status());
+        // System.out.println("Carriage Status: " + ccp_connection_man.send_Carriage_Status());
+        // System.out.println("Door State: " + ccp_connection_man.get_Door_State());
 
-        // Commanding to Open door when at station
-        ccp_connection_man.esp32_manager.door_Command("OPEN");
-        System.out.println("\n\nDoor should be set to open...\nState is: " + ccp_connection_man.get_Door_State());
+        // // Commanding to Open door when at station
+        // ccp_connection_man.esp32_manager.door_Command("OPEN");
+        // System.out.println("\n\nDoor should be set to open...\nState is: " + ccp_connection_man.get_Door_State());
+    
+        UDPTest test = new UDPTest();
+        test.setup();
+        test.testSend();
+        test.tearDown();
+        
     }
 }
