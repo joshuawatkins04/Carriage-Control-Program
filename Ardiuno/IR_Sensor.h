4c15c3489802a392407240acaf8fc684d060b48f
Connell Ng <connell.ng@students.mq.edu.au>
2024-09-02 13:20:00 +1000

    ir_sensor and motor_controller done, not implemented into the main thing

--- Ardiuno/IR_Sensor.h
+++ Ardiuno/IR_Sensor.h
@@ -1,10 +1,29 @@
+#include "Setup.h"
+
 class IR_Sensor {
+    private:
+        int ir_sensor_pin; //change to pin thats being used
 
     public:
-    
-    IR_Sensor() {}
+
+    IR_Sensor() : ir_sensor_pin(Setup::ir_sensor) {
+        pinMode(ir_sensor_pin, INPUT);
+    }
 
     bool operational() {
-        return true;
+        int sensorState = digitalRead(ir_sensor_pin);
+        return sensorState == HIGH;
     }
 };
+
+/* IMPLEMENT somewhere idk?
+void loop() {
+    if(irSensor.operational()){
+        motor.slowBR();
+        motor.stopBR();
+    } else{
+        motor.fastBR();
+    }
+    delay(500);
+}
+*/
