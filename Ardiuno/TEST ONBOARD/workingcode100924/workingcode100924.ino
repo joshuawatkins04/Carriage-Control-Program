d445a787497835710eef99c6420ce7ef472aabf2
Joshua Watkins <joshua@watkinsfamily.com.au>
2024-09-10 15:37:44 +1000

    Added wifi

--- Ardiuno/TEST ONBOARD/workingcode100924/workingcode100924.ino
+++ Ardiuno/TEST ONBOARD/workingcode100924/workingcode100924.ino
@@ -6,7 +6,28 @@
 //#define ir_sensor 
 //#define servo
 
+// #include <WiFi.h>
+// #include <WiFiUdp.h>
+
+// WiFiUDP udp;
+
+// const char* ssid = "ENGG2K3K";
+
+// IPAddress ip(10, 20, 30, 115);
+// IPAddress udpAddress(10, 20, 30, 1);
+// const int udpLocalPort = 3015;
+// const int udpAddressPort = 3016;
+
+// IPAddress dns(192, 168, 43, 1);
+// IPAddress gateway(192, 168, 43, 1);
+// IPAddress subnet(255, 255, 255, 0);
+
+// char inBuffer[255];
+// char outBuffer[255];
+
 void setup() {
+  Serial.begin(115200);
+  
   pinMode(motor_speed, OUTPUT);
   pinMode(motor_direction, OUTPUT);
   pinMode(green_led, OUTPUT);
@@ -17,9 +38,27 @@ void setup() {
   digitalWrite(green_led, LOW);
   digitalWrite(yellow_led, LOW);
   digitalWrite(red_led, LOW);
+
+  delay(1000);
+  
+  WiFi.config(ip, gateway, subnet, dns);
+  WiFi.begin(ssid);
+  Serial.println("\nConnecting");
+  
+  while(WiFi.status() != WL_CONNECTED){
+      Serial.print(".");
+      delay(100);
+  }
+  
+  Serial.println("\nConnected to the WiFi network");
+  Serial.print("[+] ESP32 IP : ");
+  Serial.println(WiFi.localIP());
+  
+  udp.begin(udpLocalPort);
 }
 
 void loop() {
+  Serial.println("\nBeginning main loop");
   //motor_test();
   //led_test();
   //servo_test();
@@ -71,3 +110,25 @@ void motor_test() {
   analogWrite(motor_speed, 255);
   delay(2000);
 }
+
+// void sendMessage(char str[255]){
+//   strcpy(outBuffer, str);
+//   udp.beginPacket(udpAddress, udpAddressPort);
+//   udp.write(outBuffer, 11);
+//   udp.endPacket();
+//   //memset(outBuffer, 0, 50);
+// }
+
+// void receiveMessage(){
+//   udp.parsePacket();
+//   if(udp.read(inBuffer, 255) > 0){
+//     Serial.print("Server to client: ");
+//     Serial.println((char *)inBuffer);
+//     if(inBuffer == "START") {
+//       sendMessage("STARTING");
+//     }
+//     if(inBuffer == "STOP") {
+//       sendMessage("STOPPING");
+//     }
+//   }
+// }
