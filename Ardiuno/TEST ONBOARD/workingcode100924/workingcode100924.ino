443015080145d7a76b1cba870ecb5c139d8f27ba
Joshua Watkins <joshua@watkinsfamily.com.au>
2024-09-10 15:34:02 +1000

    Formatted some of document

--- Ardiuno/TEST ONBOARD/workingcode100924/workingcode100924.ino
+++ Ardiuno/TEST ONBOARD/workingcode100924/workingcode100924.ino
@@ -1,76 +1,73 @@
-#define direction 23
-#define speed 22
-#define red 21
-#define green 18
-#define yellow 19
-//#define IR 
+#define motor_direction 23
+#define motor_speed 22
+#define green_led 18
+#define yellow_led 19
+#define red_led 21
+//#define ir_sensor 
 //#define servo
 
 void setup() {
-pinMode(speed,OUTPUT);
-pinMode(direction,OUTPUT);
-pinMode(red,OUTPUT);
-pinMode(green,OUTPUT);
-pinMode(yellow,OUTPUT);
-//pinMode(IR,INPUT);
-//pinMode(servo,OUTPUT);
-digitalWrite(red,LOW);
-digitalWrite(green,LOW);
-digitalWrite(yellow,LOW);
+  pinMode(motor_speed, OUTPUT);
+  pinMode(motor_direction, OUTPUT);
+  pinMode(green_led, OUTPUT);
+  pinMode(yellow_led, OUTPUT);
+  pinMode(red_led, OUTPUT);
+  //pinMode(ir_sensor, INPUT);
+  //pinMode(servo, OUTPUT);
+  digitalWrite(green_led, LOW);
+  digitalWrite(yellow_led, LOW);
+  digitalWrite(red_led, LOW);
 }
 
 void loop() {
-
-//motor_test();
-//LED_test();
-//servo_test();
-//ir_test();
+  //motor_test();
+  //led_test();
+  //servo_test();
+  //ir_test();
 }
 
-void LED_test()
-{
-digitalWrite(red,HIGH);
-digitalWrite(green,HIGH);
-digitalWrite(yellow,HIGH);
-delay(1000);
-digitalWrite(red,LOW);
-digitalWrite(green,LOW);
-digitalWrite(yellow,LOW);
-delay(1000);
+void led_test() {
+  Serial.println("\nRunning LED Test:");
+  digitalWrite(green_led,HIGH);
+  digitalWrite(yellow_led,HIGH);
+  digitalWrite(red_led,HIGH);
+  delay(1000);
+  digitalWrite(green_led,LOW);
+  digitalWrite(yellow_led,LOW);
+  digitalWrite(red_led,LOW);
+  delay(1000);
 }
 
-void ir_test()
-{
-switch(digitalRead(IR))
-case >100:
-   Serial.println("THERE IS LIGHT");
-    break;
- case <100:
-  Serial.println("NO LIGHT");
-    break;
+// void ir_test() {
+//   Serial.println("\nRunning IR Test:");
+//   switch(digitalRead(ir_sensor))
+//     case > 100:
+//       Serial.println("THERE IS LIGHT");
+//       break;
+//     case <100:
+//       Serial.println("NO LIGHT");
+//       break;
+// }
 
+// void servo_test() {
+//   Serial.println("\nRunning Servo Test:");
+//   delay(2000);
+//   digitalWrite(servo, HIGH);
+//   Serial.println("Open door");
+//   delay(2000);
+//   digitalWrite(servo, LOW);
+//   Serial.println("Close door");
+// }
 
-}
-
-void servo_test()
-{
+void motor_test() {
+  Serial.println("\nRunning Motor Test:");
+  digitalWrite(motor_direction, HIGH);
+  analogWrite(motor_speed, 255);
   delay(2000);
-  digitalWrite(servo,HIGH);
-  Serial.println("open");
+  analogWrite(motor_speed, 0);
   delay(2000);
-  digitalWrite(servo,LOW);
-   Serial.println("closed");
 
-}
-void motor_test()
-{
-digitalWrite(direction,HIGH);
-analogWrite(speed,255);
-delay(2000);
-analogWrite(speed,0);
-delay(2000);
-
-digitalWrite(direction,LOW);
-analogWrite(speed,255);
-delay(2000);
+  digitalWrite(motor_direction, LOW);
+  analogWrite(motor_speed, 255);
+  delay(2000);
 }
